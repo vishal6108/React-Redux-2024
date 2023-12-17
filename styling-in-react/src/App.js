5f3ef5b28550a4530094ec584046c4d37c18f53a
vishal6108 <vishalsingh6108@gmail.com>
2023-12-17 18:52:53 +0530

    CR: Styling React

---
+++ styling-in-react/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import CustomInputField from "./components/custom_input";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CustomInputField />
+      </main>
+    </>
+  );
+}
+
+export default App;
