680a0a8d7cb462e4baa50c3051c57d50c05d8941
vishal6108 <vishalsingh6108@gmail.com>
2023-12-21 13:19:23 +0530

    CR:'useContext Demo Project'

---
+++ context-demo-app/src/components/Header.jsx
@@ -0,0 +1,43 @@
+import { useRef, useContext } from 'react';
+import logo  from '../assets/logo.png'
+import CartModal from './CartModal.jsx';
+import { CartContext } from '../store/sopping_context.jsx';
+
+function Header() {
+  const modal = useRef();
+  const { items } = useContext(CartContext);
+
+  const cartQuantity = items.length;
+
+  function handleOpenCartClick() {
+    modal.current.open();
+  }
+
+  let modalActions = <button>Close</button>;
+
+  if (cartQuantity > 0) {
+    modalActions = (
+      <>
+        <button>Close</button>
+        <button>Checkout</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <CartModal ref={modal} title="Your Cart" actions={modalActions} />
+      <header id="main-header">
+        <div id="main-title">
+          <img src={logo} alt="Elegant model" />
+          <h1>Elegant Context</h1>
+        </div>
+        <p>
+          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
+        </p>
+      </header>
+    </>
+  );
+}
+
+export default Header
---
+++ context-demo-app/src/components/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Shop({children}) {
+  return (
+    <section id="shop">
+    <h2>Elegant Clothing For Everyone</h2>
+
+    <ul id="products">{children}</ul>
+  </section>
+  )
+}
+
+export default Shop
