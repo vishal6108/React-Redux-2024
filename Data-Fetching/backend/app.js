a9dd6570bad80d6051d1c48ed3c0e5707b702ef1
vishal6108 <vishalsingh6108@gmail.com>
2024-01-01 15:58:44 +0530

    CR: 'Sending Http Req and Connecting to a DB'

---
+++ Data-Fetching/backend/app.js
@@ -0,0 +1,54 @@
+const fs = require("node:fs/promises");
+
+const bodyParser = require("body-parser");
+const express = require("express");
+var path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "/assets")));
+console.log(path.join(__dirname, "/assets"));
+// app.use(express.static("/assets"));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  next();
+});
+
+app.get("/places", async (req, res) => {
+  const fileContent = await fs.readFile("./data/places.json");
+
+  const placesData = JSON.parse(fileContent);
+
+  res.status(200).json({ places: placesData });
+});
+
+app.get("/user-places", async (req, res) => {
+  const fileContent = await fs.readFile("./data/user-places.json");
+
+  const places = JSON.parse(fileContent);
+
+  res.status(200).json({ places });
+});
+
+app.put("/user-places", async (req, res) => {
+  const places = req.body.places;
+
+  await fs.writeFile("./data/user-places.json", JSON.stringify(places));
+
+  res.status(200).json({ message: "User places updated!" });
+});
+
+// 404
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  res.status(404).json({ message: "404 - Not Found" });
+});
+
+app.listen(3008);
